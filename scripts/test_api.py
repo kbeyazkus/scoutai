0d62d4aa8e84e0eb2667401a9a4da47e09db363e
kbeyazkus <95971961+kbeyazkus@users.noreply.github.com>
2026-03-22 03:51:23 +0100

    Update test_api.py

--- scripts/test_api.py
+++ scripts/test_api.py
@@ -1,5 +1,5 @@
 import requests
-API_KEY = "BURAYA_API_ANAHTARINI_YAZ"
+API_KEY = "CpJnwdzG4RHQIs37bRQehp2iET5I7fQLoOCUMkT7qxNF78FABDIciJ7ka6o6"
 url = f"https://api.sportmonks.com/v3/football/fixtures/19427160?include=news&api_token={API_KEY}"
 r = requests.get(url)
 print(f"[{r.status_code}] News Final Test")
